ee1b4827127a4e19daf00562600397b5366def80
Paulo Correa <pauloyaco@gmail.com>
2020-01-15 20:37:04 -0300

    configuracao de knex pra conectar com o banco de dados e configuracao de porta de servidor.

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ const server = restify.createServer({
   name: 'minha aplicacao',
   version: '1.0.0'
 });
+
+const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      user : 'root',
+      password : 'pass',
+      database : 'dbnode'
+    }
+  });
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+server.listen(8082, function () {
+  console.log('%s Ouvindo porta %s', server.name, server.url);
+});
